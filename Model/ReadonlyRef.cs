b85a322d7d471622518b72d05d490a0a4b9f2883
Santiago <s.firpo@edu.pucrs.br>
2023-05-18 23:28:12 -0300

    Implemented readonly ref

---
+++ Model/ReadonlyRef.cs
@@ -0,0 +1,17 @@
+﻿namespace ItineraryMapSolver.Model;
+
+public class ReadonlyRef<TReferencedType> where TReferencedType : struct
+{
+    public ReadonlyRef(in TReferencedType referencedStruct)
+    {
+        _value = referencedStruct;
+    }
+
+    public ref readonly TReferencedType GetValueRef()
+    {
+        return ref _value;
+    }
+
+    private readonly TReferencedType _value;
+}
+
