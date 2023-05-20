03183f27238689cede1f5e216d5159f455969803
Santiago <s.firpo@edu.pucrs.br>
2023-05-20 02:48:27 -0300

    Extracted itinerary computing method

--- Program.cs
+++ Program.cs
@@ -3,75 +3,92 @@ using ItineraryMapSolver.MapLoading;
 using ItineraryMapSolver.Model;
 using ItineraryMapSolver.Pathfinding;
 
-int SortByHarborId(KeyValuePair<int, IntVector> x1, KeyValuePair<int, IntVector> x2)
+namespace ItineraryMapSolver;
+
+public static class Program
 {
-     return x1.Key - x2.Key;
-}
-
-Console.WriteLine("Please insert the path to the map file: ");
-string? mapFilePath = Console.ReadLine();
-
-if (mapFilePath is not null)
-{
-    var loadMapResult = MapLoader.LoadGrid(mapFilePath);
-
-    if (loadMapResult.TryGetOkValue(out MapGrid grid))
+    public static void Main()
     {
-        Console.WriteLine(grid.DebugPrint());
 
-        var allDestinations = grid.Destinations.ToList();
-        allDestinations.Sort(SortByHarborId);
+        Console.WriteLine("Please insert the path to the map file: ");
+        string? mapFilePath = Console.ReadLine();
 
-        List<IntVector> completeItinerary = new();
-
-        for (int index = 0; index < allDestinations.Count;)
+        if (mapFilePath is null)
         {
-            (int _, IntVector fromPosition) = allDestinations[index];
-            int nextIndex = index < allDestinations.Count - 1 ? index + 1 : 0;
-            (int _, IntVector toPosition) = allDestinations[nextIndex];
-
-            var pathfindingResult = AStarSolver.SolvePath(grid, fromPosition, toPosition);
-
-            if (pathfindingResult.WasSuccessful())
-            {
-                var pathfindingSolution = pathfindingResult.GetOkValueUnsafe();
-                var fromHarborId = grid.GetHarborId(pathfindingSolution[0]);
-                var toHarborId = grid.GetHarborId(pathfindingSolution[^1]);
-                
-                // Console.WriteLine($"Found a path between harbor {fromHarborId.GetValue()} at position {pathfindingSolution[0]} and harbor {toHarborId.GetValue()} at position {pathfindingSolution[^1]}: ");
-
-                // Console.WriteLine(grid.DebugPrintPath(pathfindingSolution.ToHashSet()));
-
-                completeItinerary.AddRange(pathfindingSolution);
-                ++index;
-            }
-            else
-            {
-                (IntVector from, IntVector to, EPathfindingError _) = pathfindingResult.GetErrorValueUnsafe();
-                int toHarborId = grid.GetHarborId(to).GetValue();
-                // Console.WriteLine($"No path exists between harbor {grid.GetHarborId(from).GetValue()} at {from} and harbor {toHarborId} at {to}. Ignoring..");
-                allDestinations.Remove(new KeyValuePair<int, IntVector>(toHarborId, to));
-            }
+            return;
         }
+
+        var loadMapResult = MapLoader.LoadGrid(mapFilePath);
+
+        if (loadMapResult.TryGetOkValue(out MapGrid grid))
+        {
+            Console.WriteLine(grid.DebugPrint());
+
+            var allDestinations = grid.Destinations.ToList();
+            allDestinations.Sort(SortByHarborId);
+
+            List<IntVector> completeItinerary = new();
+
+            for (int index = 0; index < allDestinations.Count;)
+            {
+                SolveHarborInItinerary(allDestinations, ref index, grid, completeItinerary);
+            }
         
-        Console.WriteLine("Complete itinerary path: ");
-        Console.WriteLine(grid.DebugPrintPath(completeItinerary.ToHashSet()));
+            Console.WriteLine("Complete itinerary path: ");
+            Console.WriteLine(grid.DebugPrintPath(completeItinerary.ToHashSet()));
 
-        StringBuilder itinerary = new();
-        foreach (var destination in allDestinations)
-        {
-            itinerary.Append($"{destination.Key}");
-            itinerary.Append(" -> ");
-        }
-        if (allDestinations.Count > 0)
-        {
-            itinerary.Append(itinerary[0]);
-        }
+            StringBuilder itinerary = new();
+            foreach (var destination in allDestinations)
+            {
+                itinerary.Append($"{destination.Key}");
+                itinerary.Append(" -> ");
+            }
+            if (allDestinations.Count > 0)
+            {
+                itinerary.Append(itinerary[0]);
+            }
 
-        Console.WriteLine($"Total walking cost of itinerary ({itinerary}) is {completeItinerary.Count - 1}");
+            Console.WriteLine($"Total walking cost of itinerary ({itinerary}) is {completeItinerary.Count - 1}");
+        }
+        else
+        {
+            Console.Error.WriteLine($"Error while processing map: {loadMapResult.GetErrorValueUnsafe()}");
+        }
     }
-    else
+
+    private static void SolveHarborInItinerary(in List<KeyValuePair<int, IntVector>> allDestinations, ref int index, in MapGrid grid, in List<IntVector> completeItinerary)
     {
-        Console.Error.WriteLine($"Error while processing map: {loadMapResult.GetErrorValueUnsafe()}");
+
+        (int _, IntVector fromPosition) = allDestinations[index];
+        int nextIndex = index < allDestinations.Count - 1 ? index + 1 : 0;
+        (int _, IntVector toPosition) = allDestinations[nextIndex];
+
+        var pathfindingResult = AStarSolver.SolvePath(grid, fromPosition, toPosition);
+
+        if (pathfindingResult.WasSuccessful())
+        {
+            var pathfindingSolution = pathfindingResult.GetOkValueUnsafe();
+            var fromHarborId = grid.GetHarborId(pathfindingSolution[0]);
+            var toHarborId = grid.GetHarborId(pathfindingSolution[^1]);
+
+            // Console.WriteLine($"Found a path between harbor {fromHarborId.GetValue()} at position {pathfindingSolution[0]} and harbor {toHarborId.GetValue()} at position {pathfindingSolution[^1]}: ");
+
+            // Console.WriteLine(grid.DebugPrintPath(pathfindingSolution.ToHashSet()));
+
+            completeItinerary.AddRange(pathfindingSolution);
+            ++index;
+        }
+        else
+        {
+            (IntVector from, IntVector to, EPathfindingError _) = pathfindingResult.GetErrorValueUnsafe();
+            int toHarborId = grid.GetHarborId(to).GetValue();
+            // Console.WriteLine($"No path exists between harbor {grid.GetHarborId(from).GetValue()} at {from} and harbor {toHarborId} at {to}. Ignoring..");
+            allDestinations.Remove(new KeyValuePair<int, IntVector>(toHarborId, to));
+        }
+    }
+
+    private static int SortByHarborId(KeyValuePair<int, IntVector> x1, KeyValuePair<int, IntVector> x2)
+    {
+        return x1.Key - x2.Key;
     }
 }
